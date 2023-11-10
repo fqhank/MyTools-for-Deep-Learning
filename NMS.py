2da77d3984de19c25136b67eb723660807495534
HankFeng <67870028+fqhank@users.noreply.github.com>
2023-11-10 11:26:03 +0800

    Create NMS.py

---
+++ NMS.py
@@ -0,0 +1,24 @@
+def nms(proposals, thresh):
+    proposals = np.array(proposals)
+    x1 = proposals[:, 2]
+    x2 = proposals[:, 3]
+    scores = proposals[:, 1]
+
+    areas = x2 - x1 + 1
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(proposals[i].tolist())
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+
+        inter = np.maximum(0.0, xx2 - xx1 + 1)
+
+        iou = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(iou < thresh)[0]
+        order = order[inds + 1]
+
+    return keep
